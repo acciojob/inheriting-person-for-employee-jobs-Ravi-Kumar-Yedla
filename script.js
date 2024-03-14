964fa295e6dde6104667b8cd995261380ec8a8c3
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-14 18:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
+	this.pername=name;
+	this.pernage=age;
 	greet(){
 		console.log(Hello, my name is ${this.name}, I am ${this.age}years old.)
 	}
 }
 function Employee(name, age,jobTitle) {
-	super(name,age)
-	this.jobtitle=jobTitle;
+	this.empname=name;
+	this.empage=age;
+	this.empjobtitle=jobTitle;
 	jobGreet(){
 		console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.)
 	}
