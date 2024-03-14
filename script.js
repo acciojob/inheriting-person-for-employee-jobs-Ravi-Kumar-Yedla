d45e6e6125bd612a407b2687719ca631b5ee9adb
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-14 17:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-// complete this js code
-//your code here
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+	this.pername=name;
+	this.pernage=age;
+	greet(){
+		console.log(Hello, my name is ${this.name}, I am ${this.age}years old.)
+	}
+}
+function Employee(name, age,jobTitle) {
+	this.empname=name;
+	this.empage=age;
+	this.empjobtitle=jobTitle;
+	jobGreet(){
+		console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.)
+	}
 }
 
-Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-};
-
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
-}
-
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function () {
-  console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-  );
-};
-
+// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
