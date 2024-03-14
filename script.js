a77c649ce7188a96edf105d19e8dc7ec0b6344dc
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-14 09:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-// complete this js code
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-class Person{
-	constructor(name,age){
-	this.name=name;
-	this.age=age;
-	}
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}
-}
-class Employee extends Person{
-	constructor(name,age,jobTitle){
-		super(name,age)
-		this.jobTitle=jobTitle;
-	}
-	jobGreet(){
-console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)		
-	}
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
 }
 
-const person = new Person("Alice",25);
-person.greet();
-const employee = new Employee("Bob",30,"Manager");
-employee.jobGreet()
+// Define the Employee class that inherits from Person
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
 // function Person(name, age) {
 	
 // }
