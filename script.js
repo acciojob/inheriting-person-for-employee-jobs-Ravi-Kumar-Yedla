e1739cf2e5db8f212e197d1d3f8c5a82affbced8
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-14 17:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-
+// complete this js code
+//your code here
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
-	greet(){
-		console.log("Hello, my name is"+this.name+ ",I am "+this.age+"years old.")
-	}
+  this.name = name;
+  this.age = age;
 }
-greet()
-function Employee(name, age, jobTitle) {
-	super(name,age);
-	this.jobTitle=jobTitle;
-	jobGreet(){
-		console.log("Hello, my name is"+this.name+ ",I am "+this.age+"years old,and my job title is"+this.jobTitle+);
-	}
-}
-jobGreet()
 
-// Do not change code below this line
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
+
 window.Person = Person;
 window.Employee = Employee;
